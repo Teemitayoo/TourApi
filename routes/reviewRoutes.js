2db8c7b7cbb541ef7766d43b539c3b851c59df75
Teemitayoo <teemitayoo5@gmail.com>
2022-10-10 17:20:49 +0100

    nested routes

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true }); //to access tour id from tour routes
 router
   .route('/')
   .get(reviewController.getAllReviews)
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,28 +1,22 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
 const authController = require('./../controllers/authController');
+const reviewRouter = require('./../routes/reviewRoutes');
 const router = express.Router();
 
 // router.param('id', tourController.checkID); //PARAM MIDDLEWARE
+
+router.use('/:tourId/reviews', reviewRouter);
+
 router
   .route('/top-5-cheap')
-  .get(
-    tourController.aliasTopTours,
-    tourController.getAllTours
-  );
-router
-  .route('/monthly-plan/:year')
-  .get(tourController.getMonthlyPlan);
-router
-  .route('/tour-stats')
-  .get(tourController.getTourStats);
+  .get(tourController.aliasTopTours, tourController.getAllTours);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router.route('/tour-stats').get(tourController.getTourStats);
 
 router
   .route('/')
-  .get(
-    authController.protect,
-    tourController.getAllTours
-  )
+  .get(authController.protect, tourController.getAllTours)
   .post(tourController.createTour);
 router
   .route('/:id')
@@ -33,5 +27,12 @@ router
     authController.restrictTo('admin', 'lead-gude'),
     tourController.deleteTour
   );
+// router
+//   .route('/:tourId/reviews')
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('user'),
+//     reviewController.createReview
+//   );
 
 module.exports = router;
